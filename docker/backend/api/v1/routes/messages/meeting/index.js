7addc733131929a257a1a5ae2e57014dd7f053fd
ChukwuemekaAjaero <emeka.ajaero7@gmail.com>
2021-06-05 14:40:34 -0400

    BACK-8: Scaffold the API Routes
    *Add some more scaffold API routes for the meeting messages module

--- docker/backend/api/v1/routes/messages/meeting/index.js
+++ docker/backend/api/v1/routes/messages/meeting/index.js
@@ -3,7 +3,41 @@
 const express = require('express');
 const router = express.Router();
 
+//Home Path
+//TODO: Better organize the order of the routes
+router.get('/', (res, req, next) => {
+    res.send('NOT IMPLEMENTED: meeting messages GET');
+});
 
+//Get all the messages from a meeting using its id
+router.get('/:meeting_id', (res, req, next) => {
+    res.send('NOT IMPLEMENTED: meeting_id messages GET');
+});
+
+//Create a new message from one user to the meeting chat
+router.post('/:meeting_id/:message_from_id', (res, req, next) => {
+    res.send('NOT IMPLEMENTED: meeting_id POST from message_from_id');
+});
+
+//Get a specific message from the meeting chat
+router.get('/:meeting_id/:message_id', (res, req, next) => {
+    res.send('NOT IMPLEMENTED: meeting_id GET message_id');
+});
+
+//Delete a specific message from the meeting chat
+router.delete('/:meeting_id/:message_id', (req, res, next) => {
+    res.send('NOT IMPLEMENTED: meeting_id DELETE message_id');
+});
+
+//Get a specific message's status
+router.get('/:meeting_id/:message_id/status', (req, res, next) => {
+    res.send('NOT IMPLEMENTED: meeting_id GET message_id status');
+});
+
+//Update a specific message's status
+router.put('/:meeting_id/:message_id', (req, res, next) => {
+    res.send('NOT IMPLEMENTED: meeting_id PUT message_id status');
+});
 
 
 module.exports = router;
