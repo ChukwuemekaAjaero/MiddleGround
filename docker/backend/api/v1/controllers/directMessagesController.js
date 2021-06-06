721965954b293bb3abff5133abf5baa2083bae6b
ChukwuemekaAjaero <emeka.ajaero7@gmail.com>
2021-06-06 16:01:53 -0400

    BACK-8: Scaffold the API Routes
    *Added the scaffold for the directMessagesController
    *Replaced the callback functions in messages\direct\index.js to the functions in the directMessagesController.

---
+++ docker/backend/api/v1/controllers/directMessagesController.js
@@ -0,0 +1,43 @@
+// --------------------- THIS MODULE IS TO STORE THE CONTROLLERS FOR THE USERS API ROUTES ---------------------
+
+exports.direct_messages_list = (req, res) => {
+    //This should check who's making the request and if they have authorisation.
+    res.send('NOT IMPLEMENTED: direct messages GET');
+};
+
+//Get all the messages from a user to another user
+exports.direct_messages_user_to_user_list = (req, res) => {
+    /*
+   * An interesting thing to think about here is how to "lazy load" the messages. You wouldn't want to load every single
+   * message all at once when that's not what's needed.
+   * https://stackoverflow.com/questions/53241157/how-to-add-query-parameters-to-router-get
+   * */
+    res.send('NOT IMPLEMENTED: message from -> to GET');
+};
+
+//Create a new message from one user to another
+exports.create_new_direct_message = (req, res) => {
+    res.send('NOT IMPLEMENTED: message from -> to POST');
+};
+
+//Get a specific message from its ID.
+exports.get_direct_message = (req, res) => {
+    res.send('NOT IMPLEMENTED: message_id GET');
+};
+
+//Delete a specific message
+exports.delete_direct_message = (req, res) => {
+    res.send('NOT IMPLEMENTED: message_id DELETE');
+};
+
+//Get a message's status
+exports.get_direct_message_status = (req, res) => {
+    res.send('NOT IMPLEMENTED: message_id GET status');
+};
+
+//Update a message's status
+exports.update_direct_message_status = (req, res) => {
+    res.send('NOT IMPLEMENTED: message_id PUT status');
+};
+
+
--- docker/backend/api/v1/routes/messages/direct/index.js
+++ docker/backend/api/v1/routes/messages/direct/index.js
@@ -1,49 +1,33 @@
 // --------------------- THIS MODULE IS TO STORE ALL THE API ROUTES FOR THE DIRECT MESSAGES ROUTES ---------------------
 
+// --------------------- CONSTANTS  ---------------------
 const express = require('express');
 const router = express.Router();
+const direct_messages_controller = require('../../../controllers/directMessagesController');
 
+// --------------------- ROUTES ---------------------
 //Home Path
 //TODO: Better organize the order of the routes.
-router.get('/', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: direct messages GET');
-});
+router.get('/', direct_messages_controller.direct_messages_list);
 
 //Get all the messages from a user to another user
 //TODO: Figure out if this is the right way to structure a route where there
-router.get('/:message_from_id/:message_to_id', (req, res, next) => {
-   /*
-   * An interesting thing to think about here is how to "lazy load" the messages. You wouldn't want to load every single
-   * message all at once when that's not what's needed.
-   * https://stackoverflow.com/questions/53241157/how-to-add-query-parameters-to-router-get
-   * */
-    res.send('NOT IMPLEMENTED: message from -> to GET');
-});
+router.get('/:message_from_id/:message_to_id', direct_messages_controller.direct_messages_user_to_user_list);
 
 //Create a new message from one user to another
-router.post('/:message_from_id/:message_to_id', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: message from -> to POST');
-});
+router.post('/:message_from_id/:message_to_id', direct_messages_controller.create_new_direct_message);
 
 //Get a specific message from its ID.
-router.get('/:message_id', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: message_id GET');
-});
+router.get('/:message_id', direct_messages_controller.get_direct_message);
 
 
 //Delete a specific message
-router.delete('/:message_id', (res, req, next) => {
-    res.send('NOT IMPLEMENTED: message_id DELETE');
-});
+router.delete('/:message_id', direct_messages_controller.delete_direct_message);
 
 //Get a message's status
-router.get('/:message_id/status', (req, res, next) => {
-   res.send('NOT IMPLEMENTED: message_id GET status');
-});
+router.get('/:message_id/status', direct_messages_controller.get_direct_message_status);
 
 //Update a message's status
-router.put('/:message_id/status', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: message_id PUT status');
-})
+router.put('/:message_id/status', direct_messages_controller.update_direct_message_status);
 
 module.exports = router;
