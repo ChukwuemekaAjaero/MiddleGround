502d63675e35026c4043c2f1878606504b4a15a4
ChukwuemekaAjaero <emeka.ajaero7@gmail.com>
2021-06-06 15:22:43 -0400

    BACK-8: Scaffold the API Routes
    *Added the scaffold for the usersController
    *Replaced the callback functions in users\index.js to the functions in the userController.

--- docker/backend/api/v1/controllers/usersController.js
+++ docker/backend/api/v1/controllers/usersController.js
@@ -1,2 +1,51 @@
 // --------------------- THIS MODULE IS TO STORE THE CONTROLLERS FOR THE USERS API ROUTES ---------------------
 
+//Get all users
+exports.users_list = (req, res) => {
+    res.send('NOT IMPLEMENTED: users GET');
+};
+
+//Get a user by ID
+exports.user_by_id = (req, res) => {
+    res.send('NOT IMPLEMENTED:  users GET by Id');
+};
+
+//Register a new user
+exports.register_user = (req, res) => {
+    res.send('NOT IMPLEMENTED: user POST');
+};
+
+//Delete an existing user
+exports.delete_user = (req, res) => {
+    res.send('NOT IMPLEMENTED: user DELETE');
+};
+
+//Change an existing user's role
+exports.update_user_role = (req, res) => {
+    res.send('NOT IMPLEMENTED: user PUT role');
+};
+
+//Change an existing user's
+exports.update_user_status = (req, res) => {
+    res.send('NOT IMPLEMENTED: user PUT status');
+};
+
+//Change an existing user's password
+exports.update_user_password = (req, res) => {
+    res.send('NOT IMPLEMENTED: user PUT password');
+};
+
+//Change a user's username
+exports.update_user_username = (req, res) => {
+    res.send('NOT IMPLEMENTED: user PUT username');
+};
+
+//Change a user's first name
+exports.update_user_first_name = (req, res) => {
+    res.send('NOT IMPLEMENTED: user PUT first name');
+};
+
+//Change a user's last name
+exports.update_user_last_name = (req, res) => {
+    res.send('NOT IMPLEMENTED: user PUT last name');
+};
--- docker/backend/api/v1/routes/users/index.js
+++ docker/backend/api/v1/routes/users/index.js
@@ -1,58 +1,40 @@
 // --------------------- THIS MODULE IS TO STORE ALL THE API ROUTES FOR THE USERS ROUTE ---------------------
 
+// --------------------- CONSTANTS  ---------------------
 const express = require('express');
 const router = express.Router();
+const users_controller = require('../../controllers/usersController');
 
+// --------------------- ROUTES ---------------------
 //HOME PATH
 //TODO: Better organize the order of the routes.
-router.get('/', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: users GET');
-});
+router.get('/', users_controller.users_list);
 
 //GET User by ID
-router.get('/:id', (req, res, next) => {
-    res.send('NOT IMPLEMENTED:  users GET by Id');
-});
+router.get('/:id', users_controller.user_by_id);
 
 //Register a new user
-router.put('/:id', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user POST');
-});
+router.put('/:id', users_controller.register_user);
 
 //Delete an existing user
-router.delete('/:id', (req, res, next) => {
-   res.send('NOT IMPLEMENTED: user DELETE');
-});
+router.delete('/:id', users_controller.delete_user);
 
 //Change an existing user's role
-router.put('/:id/:role', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user PUT role');
-});
+router.put('/:id/:role', users_controller.update_user_role);
 
 //Change a user's status
-router.put('/:id/:status', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user PUT status');
-});
+router.put('/:id/:status', users_controller.update_user_status);
 
 //Change a user's password
-router.put('/:id/password', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user PUT password');
-});
+router.put('/:id/password', users_controller.update_user_password);
 
 //Change a user's username
-router.put('/:id/username', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user PUT username');
-});
+router.put('/:id/username', users_controller.update_user_username);
 
 //Change a user's first name
-router.put('/:id/firstname', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user PUT first name');
-});
+router.put('/:id/firstname', users_controller.update_user_first_name);
 
 //Change a user's last name
-router.put('/:id/lastname', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user PUT last name');
-});
-
+router.put('/:id/lastname', users_controller.update_user_last_name);
 
 module.exports = router;
