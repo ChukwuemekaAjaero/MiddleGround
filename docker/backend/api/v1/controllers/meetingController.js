76a12f68104d55a8911de4c509cefb177d15e5bc
ChukwuemekaAjaero <emeka.ajaero7@gmail.com>
2021-06-07 10:05:50 -0400

    BACK-8: Scaffold the API Routes
    *Added the scaffold for the meetingMessagesController
    *Replaced the callback functions in messages\index.js to the functions in the meetingController.

--- docker/backend/api/v1/controllers/meetingController.js
+++ docker/backend/api/v1/controllers/meetingController.js
@@ -1,1 +1,46 @@
 // --------------------- THIS MODULE IS TO STORE THE CONTROLLERS FOR THE MEETINGS API ROUTES ---------------------
+
+//Get meetings list
+exports.get_meetings_list = (req, res) => {
+    res.send('NOT IMPLEMENTED: meetings GET');
+};
+
+//Get a meeting's information
+exports.get_meeting_information = (req, res) => {
+    res.send('NOT IMPLEMENTED: meeting GET by id');
+};
+
+//Create a new meeting
+exports.create_new_meeting = (req, res) => {
+    res.send('NOT IMPLEMENTEND: meeting POST');
+};
+
+//Update a meeting's information
+exports.update_meeting_information = (req, res) => {
+    res.send('NOT IMPLEMENTED: meeting PUT updated');
+};
+
+//Delete a meeting by its id
+exports.delete_meeting = (req, res) => {
+    res.send('NOT IMPLEMENTED: meeting DELETE');
+};
+
+//Get a meeting's users
+exports.get_meeting_users = (req, res) => {
+    res.send('NOT IMPLEMENTED: meeting GET users');
+};
+
+//Update a meeting's users
+exports.update_meeting_users = (req, res) => {
+    res.send('NOT IMPLEMENTED: meeting PUT users');
+};
+
+//Get a meeting's questions
+exports.get_meeting_questions = (req, res) => {
+    res.send('NOT IMPLEMENTED: meeting GET questions');
+};
+
+//Update a meeting's questions
+exports.update_meeting_questions = (req, res) => {
+    res.send('NOT IMPLEMENTED: meeting PUT questions');
+};
--- docker/backend/api/v1/routes/meetings/index.js
+++ docker/backend/api/v1/routes/meetings/index.js
@@ -1,51 +1,36 @@
 // --------------------- THIS MODULE IS TO STORE ALL THE API ROUTES FOR THE MEETINGS ROUTES ---------------------
 
+// --------------------- CONSTANTS  ---------------------
 const express = require('express');
 const router = express.Router();
+const meetings_controller = require('../../controllers/meetingController')
 
+// --------------------- ROUTES ---------------------
 //Home path
-router.get('/', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: meetings GET');
-});
+router.get('/', meetings_controller.get_meetings_list);
 
 //Get a meeting's information
-router.get('/:meeting_id', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: meeting GET by id');
-});
+router.get('/:meeting_id', meetings_controller.get_meeting_information);
 
 //Create a new meeting
-router.post('/', (req, res, next) => {
-    res.send('NOT IMPLEMENTEND: meeting POST');
-});
+router.post('/', meetings_controller.create_new_meeting);
 
-//Update a meeting's details
-router.put('/:meeting_id', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: meeting PUT updated');
-});
+//Update a meeting's information
+router.put('/:meeting_id', meetings_controller.update_meeting_information);
 
 //Delete a meeting by its id
-router.delete('/:meeting_id', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: meeting DELETE');
-});
+router.delete('/:meeting_id', meetings_controller.delete_meeting);
 
 //Get a meeting's users
-router.get('/:meeting_id/users', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: meeting GET users');
-});
+router.get('/:meeting_id/users', meetings_controller.get_meeting_users);
 
 //Get a meeting's questions
-router.get('/meeting_id/questions', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: meeting GET questions');
-});
+router.get('/meeting_id/questions', meetings_controller.get_meeting_questions);
 
 //Update a meeting's users
-router.put('/:meeting_id/users', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: meeting GET users');
-});
+router.put('/:meeting_id/users', meetings_controller.update_meeting_users);
 
 //Update a meeting's questions
-router.put('/meeting_id/questions', (req, res, next) => {
-    res.send('NOT IMPLEMENTED: meeting GET questions');
-});
+router.put('/meeting_id/questions', meetings_controller.update_meeting_questions);
 
 module.exports = router;
